73338d7ba129c5059522b40142991540e4bd42ff
hadil244 <hadil.ahmed@hva.nl>
2024-01-18 21:12:36 +0100

    mobile menu

--- js2.js
+++ js2.js
@@ -3,13 +3,17 @@ let navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }))
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("show-nav-menu");
+    navMenu.classList.toggle("show-nav-menu");
+})
 
 
 let linkjes = document.querySelectorAll('.employee-link')
